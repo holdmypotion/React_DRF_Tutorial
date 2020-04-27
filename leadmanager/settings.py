0af5b41aee6ba23d366298ec3f8531bcac129f04
Elijah <rahulsharma240301@gmail.com>
2020-04-27 22:21:26 +0530

    Add js configs, React configs, and basic setup for dev

--- leadmanager/settings.py
+++ leadmanager/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'leads',
     'rest_framework',
+    'frontend',
 ]
 
 MIDDLEWARE = [
---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js/,
+                exclude: /node_module/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
